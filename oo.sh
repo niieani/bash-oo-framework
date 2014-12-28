f5992823fabe7a32aceed5e361b1df77c648766a
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2014-12-28 15:56:48 +0100

    clean-up

--- oo.sh
+++ oo.sh
@@ -10,11 +10,6 @@ oo:error() {
 
 ## oo functions ##
 extends() {
-#    local objectType=$1; shift
-#    local objectName=$1; shift
-#    printf ""
-#    debug oo: ${FUNCNAME[1]} is extending $1...
-
     # we can only extend when there's what to extend...
     if [ ! -z $fullName ]; then
         local extensionType=$1; shift
@@ -25,31 +20,7 @@ extends() {
     fi
 }
 
-#initialize() {
-#    # parent's object
-#    local objectType=${FUNCNAME[1]}
-#    local objectName=$1; shift
-#
-#    # generating functions starting with
-#    local methods=$(compgen -A function $objectType)
-#
-#}
-
-#oo:define() {
-#    local objectType=${FUNCNAME[1]}
-#    local objectName=$1
-#
-#    objects.add $objectType $objectName
-#}
-#
-#oo:instance() {
-#    local objectType=${FUNCNAME[1]}
-#    return ! $(objects.contains $objectType)
-#}
-
 ### REAL THING ###
-
-
 
 oo:getFullName(){
     local thisName=$1
@@ -79,7 +50,6 @@ oo:initialize(){
         "
 
     local methods=($(compgen -A function Type:$objectType::))
-#    echo ${methods[1]}
 
     debug oo: adding methods for type: $objectType
 
@@ -104,23 +74,6 @@ oo:initialize(){
     # foreach property
     #eval "$name.$property(){ local this=$fullName; $type::$method \"\$@\" }"
 }
-
-#oo:start(){
-#    debug oo: starting bash-oo /infinity/ system
-#
-#    local types=$(compgen -A function Type:)
-#    debug oo: found types [ $types ]
-#    for type in "$types"; do
-#        # trim Type: from front
-#        type=${type#*:}
-#
-#        # import methods
-#        method=$(true) Type:$type
-#
-#        # 'new' function for creating the object
-#        eval "$type() { local objectType=$type; local parentName=$fullName; local fullName=\$(oo:getFullName \$1); shift; oo:initialize \"\$@\" }"
-#    done
-#}
 
 # TODO: something is wrong with this:
 array:substract(){
@@ -201,22 +154,6 @@ oo:enableType(){
 ## add for Array ##
 # http://brizzled.clapper.org/blog/2011/10/28/a-bash-stack/
 
-## tests ##
-#Type:Animal() {
-#    printf ""
-#} && oo:enableType
-
-#Type:immutable() {
-#    printf ""
-#} && oo:enableType
-
-#Number() {
-#    # if caller - ${FUNCNAME[1]} - is also an object,
-#    # then objectName = parentName.thisName
-##    extends Object "$@"
-#    initialize "$@"
-#}
-
 debug oo: starting bash-oo /infinity/ system
 
 Type:Animal() {
@@ -288,39 +225,7 @@ Type:Human() {
 #        }
 
     fi
-
-#    $instance && Number height
-#    $instance && Number width
-#    $instance && String name
-#
-#    $instance || Type:Human::eat() {
-#        echo "$this is eating $1"
-#    }
-#
-#    $instance || Type:Human::__equals__() {
-#        echo "Checking if $this equals $1"
-#    }
-#
-#    $instance || Type:Human::__toString__() {
-#        echo "I'm a human ($this)"
-#    }
-#
-#    $instance || Type:Human::__setter__() {
-#        echo "Human is immutable"
-#    }
-#
-#    $instance || Type:Human::__getter__() {
-#        Type:Human::__toString__
-#    }
 } && oo:enableType
-
-#alias Human="objectName=$1; shift; Type:Human"
-#    if [ -z "$*" ]; then
-
-
-#alias $type=Type:$type
-
-#oo:start
 
 ## usage ##
 echo Creating Human Bazyli:
