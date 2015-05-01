44c36e8017d12e3c204994fd28fbba7c76a3fb80
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2015-05-01 16:28:49 +0200

    Replace objects with aliases for performance gains

--- lib/types/ui/03-powerline.sh
+++ lib/types/ui/03-powerline.sh
@@ -6,23 +6,28 @@ class:UnicodeString() {
 	}
 } && oo:enableType
 
+static:UI.Unicode() {
+	UI.Unicode.Print() {
+		echo -e "$1"
+	}
+} && oo:enableType
 
 static:UI.Powerline() {
     extends Object
-	UnicodeString PointingArrow = "\u27a1"
-	UnicodeString ArrowLeft = "\ue0b2"
-	UnicodeString ArrowRight = "\ue0b0"
-	UnicodeString PlusMinus = "\ue00b1"
-	UnicodeString Branch = "\ue0a0"
-	UnicodeString RefersTo = "\u27a6"
-	UnicodeString OK = "\u2714"
-	UnicodeString Fail = "\u2718"
-	UnicodeString Lightning = "\u26a1"
-	UnicodeString Cog = "\u2699"
-	UnicodeString Heart = "\u2764"
+	alias UI.Powerline.PointingArrow="UI.Unicode.Print '\u27a1'"
+	alias UI.Powerline.ArrowLeft="UI.Unicode.Print '\ue0b2'"
+	alias UI.Powerline.ArrowRight="UI.Unicode.Print '\ue0b0'"
+	alias UI.Powerline.PlusMinus="UI.Unicode.Print '\ue00b1'"
+	alias UI.Powerline.Branch="UI.Unicode.Print '\ue0a0'"
+	alias UI.Powerline.RefersTo="UI.Unicode.Print '\u27a6'"
+	alias UI.Powerline.OK="UI.Unicode.Print '\u2714'"
+	alias UI.Powerline.Fail="UI.Unicode.Print '\u2718'"
+	alias UI.Powerline.Lightning="UI.Unicode.Print '\u26a1'"
+	alias UI.Powerline.Cog="UI.Unicode.Print '\u2699'"
+	alias UI.Powerline.Heart="UI.Unicode.Print '\u2764'"
 	
 	# colorful
-	UnicodeString Star = "\u2b50"
-	UnicodeString Saxophone = $'\U1F3B7'
-	UnicodeString ThumbsUp = $'\U1F44D'
+	alias UI.Powerline.Star="UI.Unicode.Print '\u2b50'"
+	alias UI.Powerline.Saxophone="UI.Unicode.Print $'\U1F3B7'"
+	alias UI.Powerline.ThumbsUp="UI.Unicode.Print $'\U1F44D'"
 } && oo:enableType
