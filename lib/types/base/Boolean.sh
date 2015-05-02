f7763b0d973e9f9d1dc9263557f0c16bbba40d2b
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2015-05-02 15:36:25 +0200

    Fix bool setter

--- lib/types/base/Boolean.sh
+++ lib/types/base/Boolean.sh
@@ -4,8 +4,12 @@ class:Boolean() {
     extends Var
 
     methods
+        Boolean::__getter__() {
+            test ${__oo__storage["$this"]} = true
+            return $?
+        }
         Boolean::__setter__() {
-            if [[ ! -z $this ]] && ([[ $this = "true" ]] || [[ $this = "false" ]])
+            if [[ ! -z $1 ]] && ([[ $1 = "true" ]] || [[ $1 = "false" ]])
             then
                 __oo__storage["$this"]="$1"
             else
