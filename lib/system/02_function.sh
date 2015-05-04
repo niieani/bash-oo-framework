9dc815a0b03be3599ba9614d6f026519f7f8c493
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2015-05-04 14:59:00 +0200

    Cleanup

--- lib/system/02_function.sh
+++ lib/system/02_function.sh
@@ -29,49 +29,29 @@ Function.AssignParamLocally() {
     # var value is only important if making an object later on from it
     local varValue="${varDeclaration#*=}"
 
-#    echo Type \"$varType\"
-#    echo Command \"${commandWithArgs[*]}\"
-#    echo VarName \"$varName\" = $varValue
-##    echo Params \"$*\"
-#    echo Param[\"$paramNo\"] = ${!paramNo}
-#    echo -------
-
     if [[ ! -z $assignVarType ]]
     then
         local previousParamNo=$(expr $paramNo - 1)
-#        echo Executing previous $previousParamNo
-#        echo $assignVarType
 
         if [[ "$assignVarType" == "array" ]]
         then
-#            local __capture_arrLength="$assignArrLength"
-
-#            echo length: $assignArrLength
-#            local __capture_arrLength="${assignVarType%=*}"
             # passing array:
             execute="$assignVarName=( \"\${@:$previousParamNo:$assignArrLength}\" )"
-#            echo "$execute"
             eval "$execute"
             paramNo+=$(expr $assignArrLength - 1)
 
             unset assignArrLength
-
         elif [[ "$assignVarType" == "params" ]]
         then
-            #echo params
             execute="$assignVarName=( \"\${@:$previousParamNo}\" )"
             eval "$execute"
-        #${a[@]:1}
         elif [[ ! -z "${!previousParamNo}" ]]
         then
             execute="$assignVarName=\"\$$previousParamNo\""
-            #echo "$execute"
             eval "$execute"
-#            echo -------
         fi
     fi
 
-#    assignVarType="$varType"
     assignVarType="$__capture_type"
     assignVarName="$varName"
     assignArrLength="$__capture_arrLength"
@@ -81,10 +61,6 @@ Function.CaptureParams() {
     __capture_type="$_type"
     __capture_arrLength="$l"
 }
-
-declare assignVarType
-declare assignVarName
-declare assignNormalCodeStarted
 
 alias @param='Function.CaptureParams; trap "declare -i \"paramNo+=1\"; Function.AssignParamLocally \"\$BASH_COMMAND\" \"\$@\"; [[ \$assignNormalCodeStarted = true ]] && trap - DEBUG && unset assignVarType && unset assignVarName && unset assignNormalCodeStarted && unset paramNo" DEBUG; local '
 alias @mixed='_type=mixed @param'
