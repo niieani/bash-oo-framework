f332d66340387988edff777d84680d3a78ebec72
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2015-09-22 18:05:57 +0200

    how does trap really work?

---
+++ tests/debug.sh
@@ -0,0 +1,24 @@
+func() {
+  local wtf
+  echo test
+  local
+  declare --p
+}
+func
+
+#trap
+# trap '_Dbg_debug_trap_handler 0 "$BASH_COMMAND" "$@"; func' DEBUG
+# echo mama
+#
+# echo lol
+# echo works
+
+# echo test
+# echo test2
+# echo test3
+# trap
+# trap 'echo TEST' DEBUG
+# echo LALALA
+# trap - DEBUG
+# echo will it work?
+# echo yes?
