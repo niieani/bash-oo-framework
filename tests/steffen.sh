edf90c30718e05ee0433b654d37da1a8d3e5746c
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2015-12-01 16:43:33 +0100

    .idea & log test

---
+++ tests/steffen.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+## BOOTSTRAP ##
+NO_UNICODE=true source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/../lib/oo-framework.sh"
+
+namespace MyApp
+Log.AddOutput MyApp CUSTOM
+
+subject=WARN Log "I am a warning"
+subject=STEFFEN Log "I am a Steffen :-)"
