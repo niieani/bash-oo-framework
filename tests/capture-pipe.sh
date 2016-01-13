d42664d0bec94906c41ca1737c41c705f8dbf23e
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-01-13 12:52:38 +0100

    Seamless types

--- tests/capture-pipe.sh
+++ tests/capture-pipe.sh
@@ -1,15 +1,20 @@
-# shopt -s lastpipe
+#!/usr/bin/env bash
+
+shopt -s lastpipe
 
 capturePipe() {
   read -r -d '' $1
 }
+
+# shopt -s expand_aliases
+# alias capturePipe="read -r -d ''"
 
 capturePipeFaithful() {
   IFS= read -r -d '' $1
 }
 
 # declare -g awesome
-# declare awesome
+# declare awesome=
 printf "test1234\n\ntest999\n\n" | capturePipe awesome
 # echo "test999" | capturePipe awesome
 echo $awesome
