f16d3c7ef11ed118cc13e6c0ee0f93248e5c56b1
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-01-24 00:21:37 +0100

    Minify by evaling

--- build/build.sh
+++ build/build.sh
@@ -1,37 +1,56 @@
 #!/usr/bin/env bash
 
 ## BOOTSTRAP ##
-source "$( cd "${BASH_SOURCE[0]%/*}/.." && pwd )/lib/oo-framework.sh"
+# source "$( cd "${BASH_SOURCE[0]%/*}/.." && pwd )/lib/oo-framework.sh"
+# import lib/system/oo
+declare -g __oo__libPath="$( cd "${BASH_SOURCE[0]%/*}/../lib" && pwd )"
 
-minify() {
-	[string] filePath
-	[string] outputPath
-	
-	{
-		local IFS=
-		while read -r line
-		do
-			[[ $line != "System::Bootstrap" ]] && echo "$line"
-		done < "$filePath" >> "$outputFile"
-	}
-}
-
-build() {
-	[string] outputFile
-	
-	echo "#!/usr/bin/env bash" > "$outputFile"
-	echo "# oo-framework version: $(git rev-parse --short HEAD)" >> "$outputFile"
-	
-	minify "$__oo__libPath"/oo-framework.sh "$outputFile"
-	
+evalIntoMain() {
+	local blob
 	local file
-	local path
-	for file in "$__oo__libPath"/system/*.sh
+	
+	for file in "$__oo__libPath"/oo-framework.sh "$__oo__libPath"/system/*.sh "$__oo__libPath"/system/oo/*.sh
 	do
-		# minify "$file" "$outputFile"
-		cat "$file" >> "$outputFile"
-		echo >> "$outputFile"
+		blob+=$'\n'
+		blob+=$(<"$file")
 	done
+
+	eval "main() { ${blob} " $'\n' " }"
 }
 
-build ./oo-framework.sh
+evalIntoMain
+declare -f main > out-$(git rev-parse --short HEAD).sh
+
+# minify() {
+# 	[string] filePath
+# 	[string] outputPath
+#
+# 	{
+# 		local IFS=
+# 		while read -r line
+# 		do
+# 			[[ $line != "System::Bootstrap" ]] && echo "$line"
+# 		done < "$filePath" >> "$outputFile"
+# 	}
+# }
+#
+#
+# build() {
+# 	[string] outputFile
+#
+# 	echo "#!/usr/bin/env bash" > "$outputFile"
+# 	echo "# oo-framework version: $(git rev-parse --short HEAD)" >> "$outputFile"
+#
+# 	minify "$__oo__libPath"/oo-framework.sh "$outputFile"
+#
+# 	local file
+# 	local path
+# 	for file in "$__oo__libPath"/system/*.sh
+# 	do
+# 		# minify "$file" "$outputFile"
+# 		cat "$file" >> "$outputFile"
+# 		echo >> "$outputFile"
+# 	done
+# }
+
+# build ./oo-framework.sh
