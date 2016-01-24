afc7dee0809697e2d64256928ae1a7077508554e
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-01-24 17:06:09 +0100

    Update test for v2.0

--- build/test.sh
+++ build/test.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 ## BOOTSTRAP ##
-#source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/oo-framework.sh"
-
-source <(VERSION=1.0.0; URL="https://github.com/niieani/bash-oo-framework/releases/download/$VERSION/oo-framework.sh"; RETRIES=3; hash curl 2>/dev/null && curl -sL --retry $RETRIES "$URL" || wget -t $RETRIES -O - -o /dev/null "$URL" || echo "echo 'An error occured while downloading the framework.' && exit 1")
+source <(VERSION=2.0.0; URL="https://github.com/niieani/bash-oo-framework/releases/download/$VERSION/oo-bootstrap.sh"; RETRIES=3; hash curl 2>/dev/null && curl -sL --retry $RETRIES "$URL" || wget -t $RETRIES -O - -o /dev/null "$URL" || echo "echo 'An error occured while downloading the framework.' && exit 1")
 
 echo Works.
-
-yooko
